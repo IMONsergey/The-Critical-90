1d9b623e24ed60d154aefae9d21c38ebabd9fedd
Sergei I'MON <jabba.person@gmail.com>
2026-09-17 18:24:59 +0300

    Separate document links and consultation integration from interface code

---
+++ public/site-config.js
@@ -0,0 +1,8 @@
+/** Client integration points. No API keys or private values belong in this file. */
+export const siteConfig = Object.freeze({
+  reportUrl: '',
+  previewUrl: '',
+  moreReportsUrl: 'https://www.kaspersky.com/enterprise-security/resources',
+  formEndpoint: '',
+  carouselDuration: 7000,
+});
